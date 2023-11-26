7e30a2f0476d0d17075ed57591daa821e1db6c4c
Gabriel <gabriel.souzatb@hotmail.com>
2023-11-26 20:06:42 -0300

    updates login screen

--- registro_ponto/lib/main.dart
+++ registro_ponto/lib/main.dart
@@ -1,5 +1,3 @@
-// lib/main.dart
-
 import 'package:flutter/material.dart';
 import 'screens/login/login_screen.dart';
 import 'screens/registro_ponto/registro_ponto_screen.dart';
@@ -11,14 +9,17 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       routes: {
-        '/registroPonto': (context) => RegistroPontoPage(),
-        // ... outras rotas
+        '/': (context) => LoginPage(), // Rota padrão para a página de login
+        '/registroPonto': (context) => RegistroPontoPage(
+              toggleTheme: () {},
+            ),
+        // ... outras rotas que você possa adicionar
       },
       title: 'Aplicativo de Ponto',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: LoginPage(),
+      initialRoute: '/', // Define a rota inicial do seu aplicativo
     );
   }
 }
--- registro_ponto/lib/screens/registro_ponto/registro_ponto_screen.dart
+++ registro_ponto/lib/screens/registro_ponto/registro_ponto_screen.dart
@@ -1,7 +1,42 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_calendar_carousel/flutter_calendar_carousel.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool _isDarkMode = false;
+
+  void _toggleTheme() {
+    setState(() {
+      _isDarkMode = !_isDarkMode;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      localizationsDelegates: [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+      ],
+      supportedLocales: [const Locale('pt', 'BR')],
+      theme: _isDarkMode ? ThemeData.dark() : ThemeData.light(),
+      home: RegistroPontoPage(toggleTheme: _toggleTheme),
+    );
+  }
+}
 
 class RegistroPontoPage extends StatefulWidget {
+  final VoidCallback toggleTheme;
+
+  RegistroPontoPage({required this.toggleTheme});
+
   @override
   _RegistroPontoPageState createState() => _RegistroPontoPageState();
 }
@@ -15,29 +50,61 @@ class _RegistroPontoPageState extends State<RegistroPontoPage> {
     return Scaffold(
       appBar: AppBar(
         title: Text("Registro de Ponto"),
+        actions: [
+          Switch(
+            value: Theme.of(context).brightness == Brightness.dark,
+            onChanged: (value) {
+              widget.toggleTheme(); // Chama o callback para mudar o tema
+            },
+          ),
+        ],
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              child: Text('Menu'),
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+            ),
+            ListTile(
+              title: Text('Item 1'),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: Text('Item 2'),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
           children: [
-            Container(
-              height: 400, // Defina a altura desejada aqui
-              child: CalendarCarousel(
-                onDayPressed: (DateTime date, _) {
-                  setState(() {
-                    _selectedDate = date;
-                  });
-                  ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                    content: Text("Data selecionada: ${date.toLocal()}"),
-                  ));
-                },
-                weekendTextStyle: TextStyle(color: Colors.red),
-                thisMonthDayBorderColor: Colors.grey,
-                selectedDateTime: _selectedDate,
-                todayButtonColor: Colors.blue,
-                todayTextStyle: TextStyle(color: Colors.white),
-                daysHaveCircularBorder: true,
-              ),
+            CalendarCarousel(
+              locale: 'pt_BR',
+              onDayPressed: (DateTime date, _) {
+                setState(() {
+                  _selectedDate = date;
+                });
+                ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                  content: Text(
+                      "Data selecionada: ${date.day}/${date.month}/${date.year}"),
+                ));
+              },
+              weekendTextStyle: TextStyle(color: Colors.red),
+              thisMonthDayBorderColor: Colors.grey,
+              selectedDateTime: _selectedDate,
+              todayButtonColor: Colors.blue,
+              todayTextStyle: TextStyle(color: Colors.white),
+              daysHaveCircularBorder: true,
             ),
             ElevatedButton(
               child: Text("Registrar Ponto"),
