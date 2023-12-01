7a536d8b909c18557f68edf0b8cb9d81ebddadae
Gabriel <gabriel.souzatb@hotmail.com>
2023-12-01 14:56:16 -0300

    updatesBKP

--- api/src/router.js
+++ api/src/router.js
@@ -3,6 +3,7 @@ const funcionariosController = require ('./controllers/funcionariosControllers')
 const router = express.Router();
 
 router.get('/funcionarios', funcionariosController.getAll);
+router.get('/funcionarios/ativos', funcionariosController.getActives);
 
 
 module.exports = router 
